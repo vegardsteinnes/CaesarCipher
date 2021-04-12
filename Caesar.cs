c594642aac068513f80628f9e3ec7c5a10af5402
Vegard Arvid Steinnes <56077836+vegardsteinnes@users.noreply.github.com>
2021-04-12 13:33:25 +0200

    Caesar 1.0b
    Med kommentarer etc

--- Caesar.cs
+++ Caesar.cs
@@ -6,16 +6,19 @@ namespace CaesarsCode
 {
     class Caesar
     {
+        // Method for encrypting txt files.
         private static int Encrypt(int position, string shift)
         {
             return position + Int32.Parse(shift);
         }
 
+        // Method for decrypting txt files.
         private static int Decrypt(int position, string shift)
         {
             return position - Int32.Parse(shift);
         }
 
+        // Method for checking if a directory of files already exists.
         private static bool FileOrDirectoryExists(string name)
         {
             return (Directory.Exists(name) || File.Exists(name));
@@ -31,7 +34,8 @@ namespace CaesarsCode
 
             WriteLine("Welcome to Vegards Caesar Encryption Tool!");
 
-            for (int i = 0; i < 1;) // Choose file to encrypt/decrypt. Error if file doesn't exist or if it has the wrong file extension.
+            // Choose file to encrypt/decrypt. Error if file doesn't exist or if it has the wrong file extension.
+            for (int i = 0; i < 1;)
             {
                 Write("Please enter the name of your file: ");
                 fileName = ReadLine();
@@ -41,7 +45,8 @@ namespace CaesarsCode
 
             string input = System.IO.File.ReadAllText(fileName);
 
-            for (int i = 0; i < 1;) // Choose amount to shift the cypher by. Error if you don't input a number.
+            // Choose amount to shift the cypher by. Error if you don't input a number.
+            for (int i = 0; i < 1;)
             {
                 Write("Enter amount to shift the cipher by: ");
                 cipherShift = ReadLine();
@@ -49,7 +54,8 @@ namespace CaesarsCode
                 if (Int32.TryParse(cipherShift, out _)) i++;
             }
 
-            for (int i = 0; i < 1;) // Choose wether to encrypt or decrypt the text file. Error if you input anything else than "E" or "D".
+            // Choose wether to encrypt or decrypt the text file. Error if you input anything else than "E" or "D".
+            for (int i = 0; i < 1;) 
             {
                 Write(@"Press ""E"" to encrypt or ""D"" to decrypt: ");
                 encryptOrDecrypt = ReadLine();
@@ -60,6 +66,8 @@ namespace CaesarsCode
             char[] message = input.ToLower().ToCharArray();
             char[] newMessage = new char[message.Length];
 
+            // Encrypts or decrypts message by searching through the cipher array for the matching letter(s) and finds it index position, then generating
+            // the new position by adding or subtracting by the shift amount and then finding the remainder by using a modulo.
             for (int i = 0; i < message.Length; i++)
             {
                 char letter = message[i];
@@ -77,7 +85,8 @@ namespace CaesarsCode
 
             string encodedString = String.Join("", newMessage);
 
-            for (int i = 0; i < 1;) // Choose name for the new encrypted/decrypted file. Error if filename doesn't end with ".txt".
+            // Choose name for the new encrypted/decrypted file. Error if filename doesn't end with ".txt".
+            for (int i = 0; i < 1;)
             {
                 if (encryptOrDecrypt.ToLower() == "e") Write("Please enter a filename for your encrypted text: ");
                 if (encryptOrDecrypt.ToLower() == "d") Write("Please enter a filename for your decrypted text: ");
@@ -90,7 +99,6 @@ namespace CaesarsCode
 
             if (encryptOrDecrypt.ToLower() == "e") WriteLine("\nHere is your encrypted text:");
             if (encryptOrDecrypt.ToLower() == "d") WriteLine("\nHere is your decrypted text:");
-
             WriteLine(encodedString + "\n");
             Write("Press any key to close the program.");
             ReadKey();
